8e8b942b29e7982ec6161d3d537a1158158ff027
Valentin Golev <me@va1en0k.net>
2013-01-16 17:17:20 +0400

    Candidate list on index

--- uc_django_tutorial/candidates/urls.py
+++ uc_django_tutorial/candidates/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
+from uc_django_tutorial.candidates.views import CandidateListView
 
+urlpatterns = patterns('',
+    url(r'^$', CandidateListView.as_view())
 )
--- uc_django_tutorial/candidates/views.py
+++ uc_django_tutorial/candidates/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from uc_django_tutorial.candidates.models import Candidate
+
+class CandidateListView(ListView):
+    model = Candidate
