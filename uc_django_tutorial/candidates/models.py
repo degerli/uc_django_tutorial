b32b0114d16db5179a75d76cded9d2342442ec71
Valentin Golev <me@va1en0k.net>
2013-01-16 17:12:05 +0400

    Added a field for resume (:sunrise_over_mountains: see screenshot!)
    https://ucarecdn.com/92d56a14-d5e9-4fd1-8d61-65ee4b3e8c95/added_resume_field.png

--- uc_django_tutorial/candidates/models.py
+++ uc_django_tutorial/candidates/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from pyuploadcare.dj import FileField
+
+
 class Candidate(models.Model):
     name = models.CharField(max_length=120)
     about = models.TextField()
+
+    resume = FileField(blank=True, null=True)
 
     def __unicode__(self):
         return self.name
