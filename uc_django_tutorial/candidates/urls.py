a3fea4cbd5da6cb05bc8a0e950ad8d20abac4f76
Valentin Golev <me@va1en0k.net>
2013-01-16 17:36:01 +0400

    Candidate update view

--- uc_django_tutorial/candidates/urls.py
+++ uc_django_tutorial/candidates/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from uc_django_tutorial.candidates.views import CandidateListView
+from uc_django_tutorial.candidates.views import (CandidateListView,
+                                                 CandidateUpdateView)
 
 urlpatterns = patterns('',
-    url(r'^$', CandidateListView.as_view())
+    url(r'^$', CandidateListView.as_view()),
+
+    url(r'^candidate/(?P<pk>\d+)/$', CandidateUpdateView.as_view(),
+            name='candidate-update')
 )
--- uc_django_tutorial/candidates/views.py
+++ uc_django_tutorial/candidates/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 
 from uc_django_tutorial.candidates.models import Candidate
 
 class CandidateListView(ListView):
     model = Candidate
+
+class CandidateUpdateView(UpdateView):
+    model = Candidate
+    success_url = '/'
+
