75bb5666561ef008c081284b935f61e81601065f
Valentin Golev <me@va1en0k.net>
2013-01-16 16:11:08 +0400

    Template settings

--- uc_django_tutorial/settings.py
+++ uc_django_tutorial/settings.py
@@ -107,6 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.abspath('./uc_django_tutorial/templates'),
 )
 
 INSTALLED_APPS = (
